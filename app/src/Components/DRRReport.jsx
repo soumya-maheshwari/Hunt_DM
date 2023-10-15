75eefdaff2239ef4cf4f524e7bedddd72bcfdd16
Soumya Maheshwari <soumyamaheshwari011@gmail.com>
2023-10-15 21:57:52 +0530

    toasts added

--- app/src/Components/DRRReport.jsx
+++ app/src/Components/DRRReport.jsx
@@ -13,6 +13,7 @@ import {
   TableCell,
 } from "@mui/material";
 import axios from "axios";
+import { ToastContainer, toast } from "react-toastify";
 
 const DRRReport = () => {
   const [startDate, setStartDate] = useState("");
@@ -37,6 +38,44 @@ const DRRReport = () => {
   }, [startDate]);
 
   const handleCalculate = () => {
+    if (!startDate) {
+      toast.error(`Please select start date`, {
+        position: "top-right",
+        autoClose: 5000,
+        theme: "dark",
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+      });
+      return;
+    }
+
+    if (!endDate) {
+      toast.error(`Please select end date`, {
+        theme: "dark",
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+      });
+      return;
+    }
+
+    if (!leadCount) {
+      toast.error(`Please add Lead count`, {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        theme: "dark",
+        pauseOnHover: true,
+        draggable: true,
+      });
+      return;
+    }
     // Calculate the number of days between selected dates
     const startDateObj = new Date(startDate);
     const endDateObj = new Date(endDate);
@@ -75,7 +114,16 @@ const DRRReport = () => {
     axios
       .post("https://hunt-m16y.onrender.com/add", newData)
       .then((response) => {
-        console.log("Data saved:", response.data);
+        console.log("Data saved:", response);
+        toast.success(`Entry added successfully`, {
+          position: "top-right",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          theme: "dark",
+          pauseOnHover: true,
+          draggable: true,
+        });
       })
       .catch((error) => {
         console.error("Error:", error);
@@ -86,100 +134,124 @@ const DRRReport = () => {
     axios
       .get("https://hunt-m16y.onrender.com/entries")
       .then((response) => {
-        setEntries(response.data);
+        setEntries(response);
+        console.log(entries);
       })
       .catch((error) => {
         console.error("Error fetching entries:", error);
       });
   }, []);
+
+  const handleCancel = () => {
+    setStartDate("");
+    setEndDate("");
+    setMonth("");
+    setYear("");
+    setLeadCount("");
+    setNumDays("");
+    setExcludeDates("");
+    setExpectedLeadCount("");
+  };
   return (
     <>
-      <Container style={{ marginTop: "10vh", maxWidth: "90vw" }}>
-        <Paper elevation={3}>
-          <Grid container spacing={2}>
-            {/* <Grid item xs={12}>
-              <Button variant="contained" color="primary">
-                Add New
-              </Button>
-            </Grid> */}
-            <Grid item xs={12}>
-              <TableContainer>
-                <Table>
-                  <TableHead>
-                    <TableRow>
-                      <TableCell>Action</TableCell>
-                      <TableCell>ID</TableCell>
-                      <TableCell>Start Date</TableCell>
-                      <TableCell>End Date</TableCell>
-                      <TableCell>Month,Year</TableCell>
-                      <TableCell>Dates Excluded</TableCell>
-                      <TableCell>No of Days</TableCell>
-                      <TableCell>Lead Count</TableCell>
-                      <TableCell>Expected DRR</TableCell>
-                      <TableCell>Last Updated</TableCell>
-                    </TableRow>
-                  </TableHead>
-                  <TableBody>
-                    <TableRow>
-                      <TableCell>N/A</TableCell>
-                      <TableCell>{id}</TableCell>
-                      <TableCell>
-                        <TextField
-                          type="date"
-                          value={startDate}
-                          onChange={(e) => setStartDate(e.target.value)}
-                        />
-                      </TableCell>
-                      <TableCell>
-                        <TextField
-                          type="date"
-                          value={endDate}
-                          onChange={(e) => setEndDate(e.target.value)}
-                        />
-                      </TableCell>
-                      <TableCell>
-                        {month}, {year}
-                      </TableCell>
-                      <TableCell>
-                        <TextField
-                          type="date"
-                          value={excludeDates}
-                          onChange={(e) => setExcludeDates(e.target.value)}
-                        />
-                      </TableCell>
-                      <TableCell>{numDays}day</TableCell>
-                      <TableCell>
-                        <TextField
-                          type="text"
-                          value={leadCount}
-                          onChange={(e) => setLeadCount(e.target.value)}
-                        />
-                      </TableCell>
-                      <TableCell>{expectedLeadCount}</TableCell>
-                      <TableCell>
-                        <Button
-                          size="small"
-                          variant="contained"
-                          color="success"
-                          style={{
-                            marginRight: "0.5vw",
-                          }}
-                          onClick={handleCalculate}
-                        >
-                          Save
-                        </Button>
-                        <Button size="small" variant="contained" color="error">
-                          Cancel
-                        </Button>
-                      </TableCell>
-                    </TableRow>
-                  </TableBody>
-                </Table>
-              </TableContainer>
+      <div className="start">
+        <Container style={{ paddingTop: "16vh", maxWidth: "90vw" }}>
+          <Paper elevation={3}>
+            <Grid container spacing={2}>
+              <Grid item xs={12}>
+                <Button
+                  variant="contained"
+                  color="primary"
+                  onClick={handleCancel}
+                >
+                  Add New
+                </Button>
+              </Grid>
+              <Grid item xs={12}>
+                <TableContainer>
+                  <Table>
+                    <TableHead>
+                      <TableRow>
+                        <TableCell>Action</TableCell>
+                        <TableCell>ID</TableCell>
+                        <TableCell>Start Date</TableCell>
+                        <TableCell>End Date</TableCell>
+                        <TableCell>Month,Year</TableCell>
+                        <TableCell>Dates Excluded</TableCell>
+                        <TableCell>No of Days</TableCell>
+                        <TableCell>Lead Count</TableCell>
+                        <TableCell>Expected DRR</TableCell>
+                        <TableCell>Last Updated</TableCell>
+                      </TableRow>
+                    </TableHead>
+                    <TableBody>
+                      <TableRow>
+                        <TableCell>N/A</TableCell>
+                        <TableCell>{id}</TableCell>
+                        <TableCell>
+                          <TextField
+                            type="date"
+                            value={startDate}
+                            onChange={(e) => setStartDate(e.target.value)}
+                          />
+                        </TableCell>
+                        <TableCell>
+                          <TextField
+                            type="date"
+                            value={endDate}
+                            onChange={(e) => setEndDate(e.target.value)}
+                          />
+                        </TableCell>
+                        <TableCell>
+                          {month}, {year}
+                        </TableCell>
+                        <TableCell>
+                          <TextField
+                            type="date"
+                            value={excludeDates}
+                            onChange={(e) => setExcludeDates(e.target.value)}
+                          />
+                        </TableCell>
+                        <TableCell>{numDays}day</TableCell>
+                        <TableCell>
+                          <TextField
+                            type="text"
+                            value={leadCount}
+                            onChange={(e) => setLeadCount(e.target.value)}
+                          />
+                        </TableCell>
+                        <TableCell>{expectedLeadCount}</TableCell>
+                        <TableCell>
+                          <Button
+                            size="small"
+                            variant="contained"
+                            color="success"
+                            style={{
+                              marginRight: "0.5vw",
+                            }}
+                            onClick={handleCalculate}
+                          >
+                            Save
+                          </Button>
+                          <Button
+                            size="small"
+                            variant="contained"
+                            color="error"
+                            onClick={handleCancel}
+                          >
+                            Cancel
+                          </Button>
+                        </TableCell>
+                      </TableRow>
+                    </TableBody>
+                  </Table>
+                </TableContainer>
+              </Grid>
             </Grid>
-          </Grid>
-        </Paper>
-      </Container>
+          </Paper>
+        </Container>
+        <ToastContainer />
+      </div>
     </>
   );
 };
